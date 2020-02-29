2bd383d54418fcff9ebdf2095dda03fe0d3f2b78
Brian Norris <briannorris@chromium.org>
2020-02-28 17:47:31 -0800

    CHROMIUM: check_whence: add (temporary?) whitelist for existing violations
    It's a lot of yaks to shave to figure out the right approach for
    everything in here. I'd like to implement automated rules to make sure
    we don't add more, so add this for now.
    
    BUG=chromium:1020597
    TEST=`make check`
    
    Signed-off-by: Brian Norris <briannorris@chromium.org>
    Change-Id: Ia77c078da4ba6908d415858285e8d4ba5cc246e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/linux-firmware/+/2081360
    Reviewed-by: Douglas Anderson <dianders@chromium.org>

--- check_whence.py
+++ check_whence.py
@@ -39,6 +39,48 @@ def main():
     known_files = set(name for name in whence_list if not name.endswith('/')) | \
                   set(['check_whence.py', 'configure', 'Makefile',
                        'README', 'copy-firmware.sh', 'WHENCE'])
+
+    # TODO(https://crbug.com/1020597): exception list of files not yet listed
+    # properly in WHENCE. Please remove from this list as you fix up WHENCE.
+    known_files |= set([
+        'ath10k/QCA6174/hw3.0/firmware-sdio-6.bin',
+        'ath10k/QCA6174/hw3.0/notice_ath10k_firmware-sdio-6.txt',
+        'ath10k/WCN3990/hw1.0/board-2.bin',
+        'brcm/brcmfmac4356-pcie.txt',
+        'brcm/brcmfmac4371-pcie.bin',
+        'brcm/brcmfmac4371-pcie.txt',
+        'intel/eve-only-ibt-hw-37.8.10-fw-22.50.19.14.f.bseq',
+        'intel/fw_sst_0f28.bin',
+        'intel/fw_sst_0f28.bin-48kHz_i2s_master',
+        'intel/fw_sst_0f28.bin-i2s_master',
+        'intel/fw_sst_0f28.bin-i2s_slave',
+        'intel/fw_sst_0f28.bin-tdm_master',
+        'intel/fw_sst_0f28.bin-tdm_slave',
+        'intel/ibt-19-0-0.ddc',
+        'intel/ibt-19-0-0.sfi',
+        'intel/ibt-19-0-1.ddc',
+        'intel/ibt-19-0-1.sfi',
+        'iwlwifi-7265D-22.ucode',
+        'iwlwifi-7265D-27.ucode',
+        'iwlwifi-9000-pu-b0-jf-b0-43.ucode',
+        'iwlwifi-9000-pu-b0-jf-b0-46.ucode',
+        'iwlwifi-9260-th-b0-jf-b0-41.ucode',
+        'iwlwifi-9260-th-b0-jf-b0-43.ucode',
+        'iwlwifi-9260-th-b0-jf-b0-46.ucode',
+        'iwlwifi-Qu-b0-jf-b0-43.ucode',
+        'iwlwifi-Qu-c0-hr-b0-50.ucode',
+        'iwlwifi-Qu-c0-hr-b0-53.ucode',
+        'iwlwifi-QuZ-a0-hr-b0-48.ucode',
+        'iwlwifi-QuZ-a0-hr-b0-50.ucode',
+        'iwlwifi-QuZ-a0-hr-b0-53.ucode',
+        'iwlwifi-cc-a0-46.ucode',
+        'iwlwifi-cc-a0-50.ucode',
+        'iwlwifi-cc-a0-53.ucode',
+        'qca/nvm_00440302_eu.bin',
+        'qca/nvm_00440302_i2s.bin',
+        'qca/nvm_00440302_i2s_eu.bin',
+        ])
+
     known_prefixes = set(name for name in whence_list if name.endswith('/'))
     git_files = set(list_git())
 
